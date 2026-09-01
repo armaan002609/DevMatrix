00e506c313b002ad8037f7f2db4c32014d20e655
armaan2609 <armaankapoor2609@gmail.com>
2026-09-01 22:14:57 +0530

    fix: resolve JSX syntax error causing build failure

--- src/App.tsx
+++ src/App.tsx
@@ -112,8 +112,6 @@ function App() {
               </div>
             </div>
           </div>
-            </div>
-          </div>
           <div className="nav-links hidden-mobile">
             {user ? (
               <>
